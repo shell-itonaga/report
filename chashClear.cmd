0414557fbb7a1adbda313979ddd0824c721bf17d
shell-itonaga <tetsuya.itonaga@shell-techno.com>
2023-05-30 16:52:57 +0900

    Update 2023-05-30

--- chashClear.cmd
+++ chashClear.cmd
@@ -1,6 +1,6 @@
-php80cli artisan config:cache
-php80cli artisan route:cache
-php80cli artisan view:clear
-php80cli artisan cache:clear
-php80cli artisan route:clear
+php artisan config:cache
+php artisan route:cache
+php artisan view:clear
+php artisan cache:clear
+php artisan route:clear
 
